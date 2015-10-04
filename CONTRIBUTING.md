c9c5541340f5c9b1bf09a88fd54b1a1ca7ea2169
Gabin <hello@gabinaureche.com>
2015-10-04 22:04:36 +0200

    setup karma, jasmine and tests

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -8,6 +8,7 @@
 
 * `npm run build` - build the `/dist` files.
 * `npm run watch` - build the `/dist` files anytime the `src/` files change.
+* `npm run test` - launch the test suite.
 
 # Rules
 
---
+++ test/TheaterJS.spec.js
@@ -0,0 +1,13 @@
+import TheaterJS from '../src/TheaterJS'
+
+describe('TheaterJS', function () {
+  let theater;
+
+  beforeEach(() => {
+    theater = new TheaterJS({ autoplay: false })
+  })
+
+  it('should merge user\'s options with the defaults', () => {
+    expect(theater.options.autoplay).toBe(false)
+  })
+})
